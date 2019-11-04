5d3f6e08c8eb3cdedaf6b24150a19b4cf669fbad
chandler37 <30558411+chandler37@users.noreply.github.com>
2019-11-04 10:06:47 -0800

    Better coverage reports, omitting third-party code

--- pyatdllib/run_coverage.sh
+++ pyatdllib/run_coverage.sh
@@ -5,7 +5,7 @@ TOP="$(dirname "$0")"
 export PYTHONPATH=".."
 rm -f .coverage || exit 1
 for x in `find "$TOP" -name '*_test.py' -print`; do
-    coverage run -a "$x"
+    coverage run "--omit=*/site-packages/*,*/third_party/*" -a "$x"
 done && \
 coverage html -i && \
 echo "See directory htmlcov/ and see http://nedbatchelder.com/code/coverage"
